2915c227ef390bb394b54062861c69608afc58f4
nekitgam01 <nekitgam@gmail.com>
2023-02-26 21:12:11 +0500

    Basic Visual Structure App

--- app/src/main/java/ru/nekitgam/githubusersapi/MainActivity.java
+++ app/src/main/java/ru/nekitgam/githubusersapi/MainActivity.java
@@ -1,7 +1,15 @@
 package ru.nekitgam.githubusersapi;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.userlist_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_auth:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    public void buttonSearchClick(View view) {
+
     }
 }
